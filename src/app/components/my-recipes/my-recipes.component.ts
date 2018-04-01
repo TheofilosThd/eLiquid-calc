a0b249ae30dff748dce50ecf7abb6c382373318c
TheofilosThd <theotheodo@gmail.com>
2018-04-01 16:40:08 +0300

    search functionality

--- src/app/components/my-recipes/my-recipes.component.ts
+++ src/app/components/my-recipes/my-recipes.component.ts
@@ -11,6 +11,7 @@ import {recipeItem} from "../../recipeItem";
 export class MyRecipesComponent implements OnInit {
 
   recipes : recipeItem[];
+  isChecked : boolean=false;
 
   constructor(private calculatorService:CalculatorService) { }
 
@@ -36,5 +37,41 @@ export class MyRecipesComponent implements OnInit {
       });
   }
 
+  checkAll(checkId) {
+    var inputs = document.getElementsByTagName("input");
+
+      if(this.isChecked){
+        this.isChecked = false;
+          for (var i = 0; i < inputs.length; i++) {
+            inputs[i].checked = false;
+          }
+      }else if(this.isChecked == false){
+        this.isChecked = true;
+          for (var i = 0; i < inputs.length; i++) {
+            inputs[i].checked = true;
+          }
+      }
+  }
+
+  searchMyRecipes(){
+    var input,filter,table,tr,td,i;
+
+
+    input = document.getElementById('searchField');
+    filter = input.value.toUpperCase();
+    table = document.getElementById('myRecipesTable');
+    tr = table.getElementsByTagName('tr');
+
+    for(i =0;i<tr.length;i++){
+      td = tr[i].getElementsByTagName('td')[1];
+      if(td){
+        if(td.innerHTML.toUpperCase().indexOf(filter)>-1){
+          tr[i].style.display="";
+        }else {
+          tr[i].style.display= "none";
+        }
+      }
+    }
+  }
 }
 
